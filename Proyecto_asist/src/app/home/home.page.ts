779c7afa349571fedc65effb08a28b34fcfb7850
ManuDuoc <man.collao@duocuc.cl>
2022-08-31 17:57:52 -0400

    1.0.7
    Co-Authored-By: Aaron Perez <101334505+UaaronC@users.noreply.github.com>

--- Proyecto_asist/src/app/home/home.page.ts
+++ Proyecto_asist/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { NavigationExtras, Router } from '@angular/router';
+import { MenuController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,43 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private menu: MenuController) {
+  Usuario: string;
+  Password: number;
+
+  Estudiante= "Aaron";
+  contra = 1234;
+
+  Profesor ="Manuel";
+  contrasena=4321;
+
+  constructor(private menu: MenuController,public router: Router, public toastController: ToastController, public menuController : MenuController) {
     this.menu.enable(false);
   }
+  ingresar(){
+    if(this.Usuario == this.Estudiante && this.Password == this.contra){
+      let navigationExtras: NavigationExtras = {
+        state: { textoEnviado: this.Usuario}
+      }
+      this.router.navigate(['/alumno'], navigationExtras);}
 
+    else if(this.Usuario == this.Profesor && this.Password == this.contrasena){
+      let NavigationExtras: NavigationExtras ={
+        state:{ textoEnviado: this.Usuario}
+      }
+      this.router.navigate(['/menu'])
+    }
+
+    else{
+      this.presentToast("Usuario y/o Contraseña Incorrectos");
+    }
+  }
+  async presentToast(message:string, duration?:number){
+    const toast = await this.toastController.create(
+      {
+        message:message,
+        duration:duration?duration:2000
+      }
+    );
+    toast.present();
+  }
 }
