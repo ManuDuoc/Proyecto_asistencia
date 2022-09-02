de7c6c4ffa58edda74ca9695bf6cdb08d034fd36
Aaron Perez <anib.perezm@duocuc.cl>
2022-09-01 21:24:08 -0400

    1.2.4
    Co-Authored-By: ManuDuoc <101334461+ManuDuoc@users.noreply.github.com>

--- Proyecto_asist/src/app/pages/recuperar/recuperar.page.ts
+++ Proyecto_asist/src/app/pages/recuperar/recuperar.page.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { MenuController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recuperar',
   templateUrl: './recuperar.page.html',
   styleUrls: ['./recuperar.page.scss'],
 })
-export class RecuperarPage implements OnInit {
+export class RecuperarPage {
 
-  constructor() { }
+  correo: string;
 
-  ngOnInit() {
+  Estudiante= "anib.perezm@duocuc.cl";
+
+  Profesor ="man.collao@duocuc.cl";
+
+  constructor(private menu: MenuController,public router: Router, public toastController: ToastController, public menuController : MenuController) {
+    this.menu.enable(false);
   }
+  enviar(){
+    if(this.correo == 'anib.perezm@duocuc.cl'){
+      let navigationExtras: NavigationExtras = {
+        state: { textoEnviado: this.correo}
+      }
+      this.router.navigate(['/home'], navigationExtras);}
 
+    
+      this.presentToast("El correo se envio con exito.");
+    
+  }
+  async presentToast(message:string, duration?:number){
+    const toast = await this.toastController.create(
+      {
+        message:message,
+        duration:duration?duration:2000
+      }
+    );
+    toast.present();
+  }
 }
